f70f7e60e07e6c89c56a04df21d939a4ee680856
Liene <Liene@LAPTOP-C1G7S8GH>
2023-04-22 23:40:30 +0300

    saving FlightStorage progress

--- FlightPlanner/FlightPlanner/Models/Flight.cs
+++ FlightPlanner/FlightPlanner/Models/Flight.cs
@@ -8,5 +8,17 @@
         public string Carrier { get; set; }
         public string DepartureTime { get; set; }
         public string ArrivalTime { get; set; }
+        /*public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            Flight flight = (Flight)obj;
+            return From.Equals(flight.From) && To.Equals(flight.To) &&
+                   Carrier.Equals(flight.Carrier) &&
+                   DepartureTime.Equals(flight.DepartureTime) &&
+                   ArrivalTime.Equals(flight.ArrivalTime);
+        }*/
     }
 }
--- FlightPlanner/FlightPlanner/Storage/FlightStorage.cs
+++ FlightPlanner/FlightPlanner/Storage/FlightStorage.cs
@@ -9,7 +9,6 @@ namespace FlightPlanner.Storage
         private static List<Flight> _flights = new List<Flight>();
         private static int _id = 1;
         
-
         public static Flight GetFlight(int id)
         {
             return _flights.SingleOrDefault(flight => flight.Id == id);
@@ -17,7 +16,7 @@ namespace FlightPlanner.Storage
 
         public static Flight AddFlight(Flight flight)
         {
-            flight.Id = _id++;
+            flight.Id = _flights.Count;
             _flights.Add(flight);
             
             return flight;
@@ -30,15 +29,36 @@ namespace FlightPlanner.Storage
 
         public static bool CheckSameFlights(Flight flight)
         {
-            foreach (var f in _flights)
+            if (flight != null)
             {
-                if (f == flight)
+                foreach (var f in _flights)
                 {
+                    if (ToLowercaseTrim(flight.ArrivalTime) == ToLowercaseTrim(f.ArrivalTime) &&
+                        ToLowercaseTrim(flight.Carrier) == ToLowercaseTrim(f.Carrier) &&
+                        ToLowercaseTrim(flight.DepartureTime) == ToLowercaseTrim(f.DepartureTime) &&
+                        ToLowercaseTrim(flight.From.AirportCode) == ToLowercaseTrim(f.From.AirportCode) &&
+                        ToLowercaseTrim(flight.From.City) == ToLowercaseTrim(f.From.City) &&
+                        ToLowercaseTrim(flight.From.Country) == ToLowercaseTrim(f.From.Country) &&
+                        ToLowercaseTrim(flight.To.AirportCode) == ToLowercaseTrim(f.To.AirportCode) &&
+                        ToLowercaseTrim(flight.To.City) == ToLowercaseTrim(f.To.City) &&
+                        ToLowercaseTrim(flight.To.Country) == ToLowercaseTrim(f.To.Country))
+                    {
                         return true;
+                    }
                 }
             }
 
             return false;
+        }
+
+        public static string ToLowercaseTrim(string s)
+        {
+            if (s != null)
+            {
+                return s!.ToLower().Trim();
+            }
+
+            return s;
         }
     }
 }
