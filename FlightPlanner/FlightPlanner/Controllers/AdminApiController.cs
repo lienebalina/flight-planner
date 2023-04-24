7f04af03c2e264d0074f59e54968469c2f037e34
Liene <Liene@LAPTOP-C1G7S8GH>
2023-04-24 18:27:25 +0300

    saving progress

--- FlightPlanner/FlightPlanner/Controllers/AdminApiController.cs
+++ FlightPlanner/FlightPlanner/Controllers/AdminApiController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using FlightPlanner.Models;
 using FlightPlanner.Storage;
+using FlightPlanner.Checker;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -27,14 +28,40 @@ namespace FlightPlanner.Controllers
         [Route("flights")]
         public IActionResult AddFlight(Flight flight)
         {
-            if (FlightStorage.CheckSameFlights(flight))
+            if (flight != null)
             {
-                return Conflict();
+                if (FlightStorage.CheckSameFlights(flight))
+                {
+                    return Conflict();
+                }
+
+                if (FlightChecker.IsFlightValueNullOrEmpty(flight))
+                {
+                    return BadRequest();
+                }
+
+                if (FlightChecker.IsItTheSameAirport(flight))
+                {
+                    return BadRequest();
+                }
+
+                if (FlightChecker.AreTheDepartureAndArrivalDateTimesValid(flight))
+                { 
+                    return BadRequest();
+                }
             }
 
             flight = FlightStorage.AddFlight(flight);
 
             return Created("", flight);
+        }
+
+        [HttpDelete]
+        [Route("flights/{id}")]
+        public IActionResult DeleteFlight(int id)
+        {
+            FlightStorage.DeleteFlight(id);
+            return Ok();
         }
     }
 }
--- FlightPlanner/FlightPlanner/Storage/FlightStorage.cs
+++ FlightPlanner/FlightPlanner/Storage/FlightStorage.cs
@@ -7,7 +7,6 @@ namespace FlightPlanner.Storage
     public static class FlightStorage
     {
         private static List<Flight> _flights = new List<Flight>();
-        private static int _id = 1;
         
         public static Flight GetFlight(int id)
         {
@@ -29,25 +28,21 @@ namespace FlightPlanner.Storage
 
         public static bool CheckSameFlights(Flight flight)
         {
-            if (flight != null)
+            foreach (var f in _flights)
             {
-                foreach (var f in _flights)
+                if (ToLowercaseTrim(flight.ArrivalTime) == ToLowercaseTrim(f.ArrivalTime) &&
+                    ToLowercaseTrim(flight.Carrier) == ToLowercaseTrim(f.Carrier) &&
+                    ToLowercaseTrim(flight.DepartureTime) == ToLowercaseTrim(f.DepartureTime) &&
+                    ToLowercaseTrim(flight.From.AirportCode) == ToLowercaseTrim(f.From.AirportCode) &&
+                    ToLowercaseTrim(flight.From.City) == ToLowercaseTrim(f.From.City) &&
+                    ToLowercaseTrim(flight.From.Country) == ToLowercaseTrim(f.From.Country) &&
+                    ToLowercaseTrim(flight.To.AirportCode) == ToLowercaseTrim(f.To.AirportCode) &&
+                    ToLowercaseTrim(flight.To.City) == ToLowercaseTrim(f.To.City) &&
+                    ToLowercaseTrim(flight.To.Country) == ToLowercaseTrim(f.To.Country))
                 {
-                    if (ToLowercaseTrim(flight.ArrivalTime) == ToLowercaseTrim(f.ArrivalTime) &&
-                        ToLowercaseTrim(flight.Carrier) == ToLowercaseTrim(f.Carrier) &&
-                        ToLowercaseTrim(flight.DepartureTime) == ToLowercaseTrim(f.DepartureTime) &&
-                        ToLowercaseTrim(flight.From.AirportCode) == ToLowercaseTrim(f.From.AirportCode) &&
-                        ToLowercaseTrim(flight.From.City) == ToLowercaseTrim(f.From.City) &&
-                        ToLowercaseTrim(flight.From.Country) == ToLowercaseTrim(f.From.Country) &&
-                        ToLowercaseTrim(flight.To.AirportCode) == ToLowercaseTrim(f.To.AirportCode) &&
-                        ToLowercaseTrim(flight.To.City) == ToLowercaseTrim(f.To.City) &&
-                        ToLowercaseTrim(flight.To.Country) == ToLowercaseTrim(f.To.Country))
-                    {
                         return true;
-                    }
                 }
             }
-
             return false;
         }
 
@@ -59,6 +54,24 @@ namespace FlightPlanner.Storage
             }
 
             return s;
+        }
+
+        public static void DeleteFlight(int id)
+        {
+            var flightIndex = 0;
+
+            if (id <= _flights.Count && id >= 0)
+            {
+                for (int i = 0; i < _flights.Count; i++)
+                {
+                    if (id == _flights[i].Id)
+                    {
+                        flightIndex = i;
+                    }
+                }
+
+                _flights.RemoveAt(flightIndex);
+            }
         }
     }
 }
